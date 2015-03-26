eb634c533b29b7672b5330e7ec4c7561fc7ff316
Patchi92 <lechieu@hotmail.com>
2015-03-26 15:54:41 +0100

    Mail Project

--- Unity/Comic Book/Assets/EvuControl.cs
+++ Unity/Comic Book/Assets/EvuControl.cs
@@ -1,9 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
-using UnityEngine.iOS;
+//using UnityEngine.iOS;
+
+using System;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
 
 public class EvuControl : MonoBehaviour {
+
+	string path;
+
+
 
 	bool oneOne;
 	bool oneTwo;
@@ -21,12 +31,19 @@ public class EvuControl : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 
+		path = Application.dataPath;
 
+
+		/*
 		//create Folder
-		if (!System.IO.Directory.Exists ("C:/Data/")) {
+		if (!System.IO.Directory.Exists ("C:/DataComicMed6/")) {
 			
-			System.IO.Directory.CreateDirectory ("C:/Data/");
+			System.IO.Directory.CreateDirectory ("C:/DataComicMed6/");
+		
 		}
+		*/
+
+
 
 
 
@@ -39,6 +56,7 @@ public class EvuControl : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+	
 
 		if(oneOne == true) {
 
@@ -56,7 +74,59 @@ public class EvuControl : MonoBehaviour {
 	}
 
 
-	void OnApplicationQuit() {
-		System.IO.File.WriteAllText("C:/Data/TestToUs.txt", One + "    " + One );
+
+	public void sendMail () {
+
+		System.IO.File.WriteAllText( path + "/TestToUs.txt", One + "    " + One );
+
+
+
+	
+
+
+		MailMessage mail = new MailMessage();
+
+
+
+		mail.From = new MailAddress("comicmed6@hotmail.com");
+		mail.To.Add("comicmed6@hotmail.com");
+		mail.Subject = "Test Mail";
+		mail.Body = "Test test test";
+
+
+
+		System.Net.Mail.Attachment attachment;
+		attachment = new System.Net.Mail.Attachment(path + "/TestToUs.txt");
+		mail.Attachments.Add(attachment);
+
+
+		
+		SmtpClient smtpServer = new SmtpClient("smtp.live.com");
+		smtpServer.Port = 587;
+		smtpServer.Credentials = new System.Net.NetworkCredential("comicmed6@hotmail.com", "TestForUs!") as ICredentialsByHost;
+		smtpServer.EnableSsl = true;
+		ServicePointManager.ServerCertificateValidationCallback = 
+			delegate(object s, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors) 
+		{ return true; };
+		smtpServer.Send(mail);
+		Debug.Log("success");
+
+
+		mailDone();
+
+
+
+	}
+
+	void mailDone () {
+
+		GetComponent<AudioSource>().Play();
+
+	
+	}
+
+
+	public void OnApplicationQuit() {
+
 	}
 }
